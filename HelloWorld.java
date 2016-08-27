7b1ea6c5c21d8a6d1e69b82572f0043567cf5554
Jerome Santos <Jerome Santos>
2016-08-26 18:33:44 -0700

    Jerome--HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		// Print to the user "Hello World!"
+		System.out.println("Hello World!");
+	}	// End main
+}	// End Program
